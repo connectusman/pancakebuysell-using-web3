edc532c5fbab14dee4591fa98b8ae4bb4fa050a5
connectshani <mushani101@gmail.com>
2021-11-18 03:36:21 +0500

    asd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express();
 const cors=require('cors');
 app.use(cors());
 app.use(express.json())
-const port=process.env.PORT|5000;
+const port=80;
 app.listen(port,()=>{
     console.log("listening on ",port)
 })
